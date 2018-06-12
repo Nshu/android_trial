12500e3628d40423e69fd55e8e58eab7451612bd
Nshu <kukkii09@gmail.com>
2018-06-12 21:32:38 +0900

    first

--- trial/src/main/java/com/example/abata/trial/trial/MainActivity.java
+++ trial/src/main/java/com/example/abata/trial/trial/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.abata.trial.trial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
     }
 }
